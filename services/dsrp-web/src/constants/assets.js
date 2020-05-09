89bfe998ba5edb3f2a620a63aae460f139aa49ae
Ryan <ryan_rondeau@live.ca>
2020-05-08 18:22:49 -0700

    casing changes

--- services/dsrp-web/src/constants/assets.js
+++ services/dsrp-web/src/constants/assets.js
@@ -1,2 +1,2 @@
 export { default as BC_GOV } from "../assets/images/logo.svg";
-export { default as MENU } from "../assets/images/menu.svg";
+export { default as MENU } from "../assets/images/Menu.svg";
