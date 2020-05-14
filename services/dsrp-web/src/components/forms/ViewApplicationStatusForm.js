26671acf083d7d68f67cfcbfe94aed1c9b7e6492
Jason Syrotuck <jasyrotuck@gmail.com>
2020-05-14 13:06:26 -0700

    form file

---
+++ services/dsrp-web/src/components/forms/ViewApplicationStatusForm.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+import React, { Component } from "react";
+import { reduxForm, Field, FormSection } from "redux-form";
+import { Row, Col, Typography, Form, Button } from "antd";
+import PropTypes from "prop-types";
+
+import { renderConfig } from "@/components/common/config";
+import { required } from "@/utils/validate";
+import { guidMask } from "@/utils/helpers";
+import * as FORM from "@/constants/forms";
+
+
+const propTypes = {
+  onChange: PropTypes.func
+};
+
+const defaultProps = {
+  onChange:()=>{},
+};
+
+class ViewApplicationStatusForm extends Component {
+  render() {
+    return (
+      <Form layout="vertical" onSubmit={this.props.handleSubmit}>
+          <Row gutter={48}>
+            <Col span={24}>
+              <Field
+                id="guid"
+                name="guid"
+                label='Application Reference Number'
+                  
+                placeholder="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+                component={renderConfig.FIELD}
+                validate={[required]}
+                {...guidMask}
+              />
+          </Col>
+        </Row>
+        <Row className="steps-action">
+          <Col>
+            <Button type="primary" htmlType="submit">
+              Check Status
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+}
+
+ViewApplicationStatusForm.propTypes = propTypes;
+ViewApplicationStatusForm.defaultProps = defaultProps;
+
+export default reduxForm({
+  form: FORM.VIEW_APPLICATION_STATUS_FORM,
+})(ViewApplicationStatusForm);
