cc14e18d8bbc5779102f85e76e2d7c4649de36bf
Luke Mitchell <lukegamitchell@gmail.com>
2020-05-14 10:24:07 -0700

    Backend sorting supported for submission_date only atm

--- services/dsrp-web/src/components/admin/ReviewApplicationInfo.js
+++ services/dsrp-web/src/components/admin/ReviewApplicationInfo.js
@@ -5,46 +5,38 @@ import { bindActionCreators } from "redux";
 import { Table } from "antd";
 import { getApplications } from "@/selectors/applicationSelectors";
 import { fetchApplications } from "@/actionCreators/applicationActionCreator";
-import { formatDateTime } from "@/utils/helpers";
+import { formatDateTime, dateSorter, nullableStringSorter } from "@/utils/helpers";
 
 const columns = [
   {
     title: "Company Name",
     dataIndex: "company_name",
-    sorter: (a, b) => a.company_name.length - b.company_name.length,
-    sortDirections: ["descend"],
     render: (text) => <div title="company_name">{text || "N/A"}</div>,
   },
   {
     title: "Permit Holder",
     dataIndex: "permit_holder",
-    defaultSortOrder: "descend",
-    sorter: (a, b) => a.permit_holder - b.permit_holder,
   },
   {
     title: "No. Wells",
     dataIndex: "wells",
-    sorter: (a, b) => a.wells.length - b.wells.length,
   },
   {
     title: "Estimated Cost",
     dataIndex: "wells",
-    sorter: (a, b) => a.wells.length - b.wells.length,
   },
   {
     title: "Eligable amount",
     dataIndex: "wells",
-    sorter: (a, b) => a.wells.length - b.wells.length,
   },
   {
     title: "Total 10% Payment",
     dataIndex: "wells",
-    sorter: (a, b) => a.wells.length - b.wells.length,
   },
   {
     title: "Submission Date",
     dataIndex: "submission_date",
-    sorter: (a, b) => a.submissionDate.length - b.submissionDate.length,
+    sorter: true,
     render: (text) => <div title="submission_date">{formatDateTime(text)}</div>,
   },
 ];
