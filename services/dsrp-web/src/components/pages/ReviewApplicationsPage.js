317db48b1d362730311d0ed01c0343f9f5145b94
Meredith Mumby <meredith.mumby@gmail.com>
2020-05-18 22:30:35 -0700

    updated description of view

--- services/dsrp-web/src/components/pages/ReviewApplicationsPage.js
+++ services/dsrp-web/src/components/pages/ReviewApplicationsPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Typography } from "antd";
+import { Row, Col, Typography, Icon } from "antd";
 import { AuthorizationGuard } from "@/hoc/AuthorizationGuard";
 import ReviewApplicationInfo from "@/components/admin/ReviewApplicationInfo";
 
@@ -11,9 +11,10 @@ export const ReviewApplicationsPage = () => (
       <Col xl={{ span: 24 }} xxl={{ span: 20 }}>
         <Title>Review Applications</Title>
         <Paragraph>
-          Integer dapibus turpis quis odio bibendum sagittis. Nunc sed vehicula ex. Praesent
-          pharetra lectus at dignissim pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit.
+          This table shows all of the applications received for the program. To review the
+          individual work components, click <Icon type="plus-square" /> next to the Well
+          Authorization Number. Click <Icon type="eye" /> to open the original application or view
+          the documents related to the application.
         </Paragraph>
       </Col>
     </Row>
