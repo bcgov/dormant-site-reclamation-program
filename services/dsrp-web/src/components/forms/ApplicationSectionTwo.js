c6cd990650985005d9e8454933b7403b9792d1fd
Luke Mitchell <lukegamitchell@gmail.com>
2020-05-20 10:47:12 -0700

    restrict well auth number to 5 digits (0-9)

--- services/dsrp-web/src/components/forms/ApplicationSectionTwo.js
+++ services/dsrp-web/src/components/forms/ApplicationSectionTwo.js
@@ -10,7 +10,12 @@ import { renderConfig } from "@/components/common/config";
 import { required } from "@/utils/validate";
 import * as FORM from "@/constants/forms";
 import { PROGRAM_START_DATE, PROGRAM_END_DATE } from "@/constants/strings";
-import { currencyMask, formatMoney, scrollToFirstError } from "@/utils/helpers";
+import {
+  currencyMask,
+  formatMoney,
+  scrollToFirstError,
+  wellAuthorizationNumberMask,
+} from "@/utils/helpers";
 import CONTRACT_WORK_SECTIONS from "@/constants/contract_work_sections";
 import PermitHolderSelect from "@/components/forms/PermitHolderSelect";
 import ApplicationFormReset from "@/components/forms/ApplicationFormReset";
@@ -404,6 +409,7 @@ class ApplicationSectionTwo extends Component {
                           )}
                         </>
                       }
+                      {...wellAuthorizationNumberMask}
                     />
                   </Col>
                 </Row>
