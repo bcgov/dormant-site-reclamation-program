90f938e2689dd951c117b0313892094b5cb302b0
Jason Syrotuck <jasyrotuck@gmail.com>
2020-05-20 15:14:51 -0700

    table create and read, just ledger

--- services/dsrp-api/app/api/application/models/application_document.py
+++ services/dsrp-api/app/api/application/models/application_document.py
@@ -11,25 +11,24 @@ from app.api.utils.models_mixins import AuditMixin, Base
 
 
 class ApplicationDocument(AuditMixin, Base):
+    """
+    Local ledger of files in S3 object store and their business relationship
+    """
+
     __tablename__ = 'application_document'
 
     class _ModelSchema(Base._ModelSchema):
         application_document_id = fields.Integer(dump_only=True)
         application_document_guid = fields.String(dump_only=True)
 
-    application_document_id = db.Column(db.Integer,
-                                        nullable=False,
-                                        unique=True,
-                                        server_default=FetchedValue())
-    application_document_guid = db.Column(UUID(as_uuid=True),
-                                          primary_key=True,
-                                          server_default=FetchedValue())
+    application_document_guid = db.Column(
+        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue())
     application_guid = db.Column(UUID(as_uuid=True), db.ForeignKey('application.guid'))
-    document_manager_guid = db.Column(UUID(as_uuid=True))
-    document_name = db.Column(db.String(255), nullable=False)
+    document_name = db.Column(db.String)
+    object_store_path = db.Column(db.String)
 
-    active_ind = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
     upload_date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
+    active_ind = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
 
     @classmethod
     def find_by_application_guid(cls, application_guid):
