d24feb72585414a8613f3e01431835acc189ca3e
Jason Syrotuck <jasyrotuck@gmail.com>
2020-05-21 14:43:18 -0700

    filename

--- services/dsrp-web/src/utils/actionlessNetworkCalls.js
+++ services/dsrp-web/src/utils/actionlessNetworkCalls.js
@@ -3,7 +3,7 @@ import { createRequestHeader } from "./requestHeaders";
 import { ENVIRONMENT } from "../constants/environment";
 import { APPLICATION_DOCUMENT } from "../constants/api";
 
-export const downloadApplicationDocument = (application_guid, document_guid) => {
+export const downloadApplicationDocument = (application_guid, document_guid, document_name) => {
   CustomAxios()
     .get(
       `${ENVIRONMENT.apiUrl + APPLICATION_DOCUMENT(application_guid)}/${document_guid}`,
@@ -13,7 +13,7 @@ export const downloadApplicationDocument = (application_guid, document_guid) => 
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "file.pdf"); //or any other extension
+      link.setAttribute("download", document_name); // or any other extension
       document.body.appendChild(link);
       link.click();
     });
