89405f07228903d2d929957d5619ead3907b7354
Paul Dittaro <pdittaro@gmail.com>
2020-05-22 17:41:06 -0700

    Location upload update

--- openshift/pipeline/config-dev.groovy
+++ openshift/pipeline/config-dev.groovy
@@ -232,7 +232,7 @@ environments {
             keycloak {
                 clientId_dsrp = "dormant-application-dev"
                 resource = "dormant-application-dev"
-                idpHint_dsrp = "dev"
+                idpHint_dsrp = "idir"
                 url = "https://sso-test.pathfinder.gov.bc.ca/auth"
                 known_config_url = "https://sso-test.pathfinder.gov.bc.ca/auth/realms/hud2v882/.well-known/openid-configuration"
                 siteminder_url = "https://logontest7.gov.bc.ca"
