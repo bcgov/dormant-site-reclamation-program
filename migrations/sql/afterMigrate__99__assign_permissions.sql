b92ec04406d60a96f2af541f3aed69b09911e385
Paul Dittaro <pdittaro@gmail.com>
2020-05-22 18:19:50 -0700

    Remove docman migration references

--- migrations/sql/afterMigrate__99__assign_permissions.sql
+++ migrations/sql/afterMigrate__99__assign_permissions.sql
@@ -7,13 +7,6 @@ GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dsrp;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO dsrp;
 
 /**
-Allow Document Manager user to CRUD anything on the docman schema
-**/
-GRANT ALL PRIVILEGES ON SCHEMA docman TO docman;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA docman TO docman;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA docman TO docman;
-
-/**
 Allow metabase user to READ anything on the dsrp schema
 **/
 GRANT USAGE ON SCHEMA public TO metabase;
