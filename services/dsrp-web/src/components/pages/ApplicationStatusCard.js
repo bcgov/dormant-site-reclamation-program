75a17d0eb56d3702d8eb7beaa3d5307397767cf4
Meredith Mumby <meredith.mumby@gmail.com>
2020-05-23 13:57:57 -0700

    updated status view to include email

--- services/dsrp-web/src/components/pages/ApplicationStatusCard.js
+++ services/dsrp-web/src/components/pages/ApplicationStatusCard.js
@@ -21,13 +21,18 @@ export const ApplicationStatusCard = (props) => (
     <Col xl={{ span: 24 }} xxl={{ span: 20 }}>
       <Result
         icon={<Icon type="info-circle" theme="twoTone" />}
-        title={`Your application is: ${
+        title={`Application Status: ${
           props.applicationStatusHash[props.application.application_status_code]
         }`}
       />
       <Typography>
-        <Paragraph>Submission Date: {formatDateTime(props.application.submission_date)}</Paragraph>
+        <Paragraph>Received On: {formatDateTime(props.application.submission_date)}</Paragraph>
       </Typography>
+      <Paragraph>
+        If you have any questions regarding your application,{" "}
+        <a href="mailto:DormantSite.BC.Government@gov.bc.ca">Contact us</a> and be sure to include
+        your reference number
+      </Paragraph>
     </Col>
   </Row>
 );
