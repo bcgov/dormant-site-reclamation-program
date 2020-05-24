f4a0cb19035b2f4729759a1ae293c3d4f9c1f5ed
Ryan <ryan_rondeau@live.ca>
2020-05-23 17:03:36 -0700

    add check another application button

--- services/dsrp-web/src/components/pages/ViewApplicationStatusPage.js
+++ services/dsrp-web/src/components/pages/ViewApplicationStatusPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Row, Col, Typography } from "antd";
+import { Row, Col, Typography, Button } from "antd";
 import PropTypes from "prop-types";
 import { bindActionCreators, compose } from "redux";
 import { withRouter } from "react-router-dom";
@@ -24,13 +24,17 @@ const propTypes = {
     application_status_code: PropTypes.string,
     submission_date: PropTypes.string,
     json: PropTypes.any,
-  }).isRequired,
+  }),
   match: PropTypes.shape({
     params: {
       id: PropTypes.string,
     },
   }).isRequired,
   history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
+
+const defaultProps = {
+  loadedApplication: { guid: "" },
 };
 
 const isGuid = (input) => {
@@ -42,6 +46,7 @@ const isGuid = (input) => {
 };
 
 export class ViewApplicationStatusPage extends Component {
+  state = { guid: "" };
   componentDidMount = () => {
     if (
       this.props.match &&
@@ -57,7 +62,7 @@ export class ViewApplicationStatusPage extends Component {
   };
 
   render = () =>
-    isEmpty(this.props.loadedApplication) ? (
+    this.props.loadedApplication.guid !== this.state.guid ? (
       <>
         <Row type="flex" justify="center" align="top" className="landing-header">
           <Col xl={{ span: 24 }} xxl={{ span: 20 }}>
@@ -80,12 +85,14 @@ export class ViewApplicationStatusPage extends Component {
               <DocumentUploadForm applicationGuid={this.props.loadedApplication.guid} />
             </>
           )}
+          <Button onClick={() => this.setState({ guid: "" })}>Check another Application</Button>
         </Col>
       </Row>
     );
 }
 
 ViewApplicationStatusPage.propTypes = propTypes;
+ViewApplicationStatusPage.defaultProps = defaultProps;
 
 const mapStateToProps = (state) => ({
   loadedApplication: getApplication(state),
