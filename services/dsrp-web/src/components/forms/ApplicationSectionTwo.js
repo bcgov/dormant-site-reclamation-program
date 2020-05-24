6e226aa0467fd335ad5329eba736cb48c1dd60d4
Luke Mitchell <lukegamitchell@gmail.com>
2020-05-24 10:50:19 -0700

    Date validation for start and end date

--- services/dsrp-web/src/components/forms/ApplicationSectionTwo.js
+++ services/dsrp-web/src/components/forms/ApplicationSectionTwo.js
@@ -125,6 +125,29 @@ const renderContractWorkPanel = (
                     (endDate && selectedDate > endDate))
                 );
               }}
+              validate={(date) => {
+                const selectedDate = date ? moment(date) : null;
+                const contractWorkValues = wellSiteFormValues.contracted_work;
+                const sectionValues = contractWorkValues
+                  ? contractWorkValues[contractWorkSection.formSectionName]
+                  : null;
+                const endDate =
+                  sectionValues && sectionValues.planned_end_date
+                    ? moment(sectionValues.planned_end_date)
+                    : null;
+                if (selectedDate) {
+                  if (
+                    selectedDate < moment(PROGRAM_START_DATE, "YYYY-MM-DD") ||
+                    selectedDate > moment(PROGRAM_END_DATE, "YYYY-MM-DD")
+                  ) {
+                    return "Date cannot be outside of the program";
+                  }
+                  if (endDate && selectedDate > endDate) {
+                    return "Planned start date cannot be after end date";
+                  }
+                }
+                return undefined;
+              }}
             />
           </Col>
           <Col span={12}>
@@ -151,6 +174,29 @@ const renderContractWorkPanel = (
                     selectedDate > moment(PROGRAM_END_DATE, "YYYY-MM-DD") ||
                     (startDate && selectedDate < startDate))
                 );
+              }}
+              validate={(date) => {
+                const selectedDate = date ? moment(date) : null;
+                const contractWorkValues = wellSiteFormValues.contracted_work;
+                const sectionValues = contractWorkValues
+                  ? contractWorkValues[contractWorkSection.formSectionName]
+                  : null;
+                const startDate =
+                  sectionValues && sectionValues.planned_start_date
+                    ? moment(sectionValues.planned_start_date)
+                    : null;
+                if (selectedDate) {
+                  if (
+                    selectedDate < moment(PROGRAM_START_DATE, "YYYY-MM-DD") ||
+                    selectedDate > moment(PROGRAM_END_DATE, "YYYY-MM-DD")
+                  ) {
+                    return "Date cannot be outside of the program";
+                  }
+                  if (startDate && selectedDate < startDate) {
+                    return "Planned end date cannot be before start date";
+                  }
+                }
+                return undefined;
               }}
             />
           </Col>
