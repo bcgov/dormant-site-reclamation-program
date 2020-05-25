1ddee0628eb456e09920841e65cf5572a994ab18
Luke Mitchell <lukegamitchell@gmail.com>
2020-05-25 13:08:32 -0700

    Increase OrgBook search debounce time to 2 seconds

--- services/dsrp-web/src/components/common/OrgBookSearch.js
+++ services/dsrp-web/src/components/common/OrgBookSearch.js
@@ -26,7 +26,7 @@ const defaultProps = {
 export class OrgBookSearch extends Component {
   constructor(props) {
     super(props);
-    this.searchOrgBookDebounced = debounce(this.props.searchOrgBook, 300);
+    this.searchOrgBookDebounced = debounce(this.props.searchOrgBook, 2000);
   }
 
   state = {
