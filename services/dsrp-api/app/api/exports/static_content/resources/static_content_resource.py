b434b2947b7a9100ea4bd73f09f52164972eb981
Luke Mitchell <lukegamitchell@gmail.com>
2020-05-25 11:48:02 -0700

    Raise an internal server error if static content failed to generate (instead of assert message)

--- services/dsrp-api/app/api/exports/static_content/resources/static_content_resource.py
+++ services/dsrp-api/app/api/exports/static_content/resources/static_content_resource.py
@@ -3,14 +3,12 @@ import json
 from flask import Response, current_app, make_response
 from flask_restplus import Resource, marshal
 from sqlalchemy.inspection import inspect
+from werkzeug.exceptions import InternalServerError
 
 from app.extensions import api, cache
-
 from app.api.utils.access_decorators import requires_role_view_all
 from app.api.constants import TIMEOUT_60_MINUTES, STATIC_CONTENT_KEY
-
 from app.api.exports.response_models import STATIC_CONTENT
-
 from app.api.application.models.application_status import ApplicationStatus
 from app.api.contracted_work.models.contracted_work_status import ContractedWorkStatus
 
@@ -36,7 +34,8 @@ class StaticContentResource(Resource):
         if not content_json:
             current_app.logger.debug('CACHE MISS - static-content')
             content = generate_static_content_dict()
-            assert content, "Generating static content failed"
+            if not content:
+                raise InternalServerError("Application failed to generate static content")
             content_dict = marshal(content, STATIC_CONTENT)
             content_json = json.dumps(content_dict, separators=(',', ':'))
             cache.set(STATIC_CONTENT_KEY, content_json, TIMEOUT_60_MINUTES)
