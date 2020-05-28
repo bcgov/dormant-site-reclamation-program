44a2477dd0a0cdf61ac10a417f1e36078fd456cd
Jason Syrotuck <jasyrotuck@gmail.com>
2020-05-28 11:45:02 -0700

    token redepemtion

--- services/dsrp-api/app/api/application/resources/gen_application_docs.py
+++ services/dsrp-api/app/api/application/resources/gen_application_docs.py
@@ -1,19 +1,24 @@
-import os
+import os, uuid
 
+from flask_restplus import Resource, reqparse
 from flask import Response, stream_with_context, current_app
-from flask_restplus import Resource
-from app.extensions import api
+from app.extensions import api, cache
 from werkzeug.exceptions import BadRequest, NotFound
 
 from app.api.utils.access_decorators import requires_role_view_all,requires_role_admin
 from app.api.utils.resources_mixins import UserMixin
+from app.api.constants import DOWNLOAD_TOKEN, TIMEOUT_5_MINUTES
+from app.api.documents.response_models import DOWNLOAD_TOKEN_MODEL
+
 
 from app.api.services.document_generator_service import DocumentGeneratorService
 from app.api.application.models.application import Application
 
-templates_base_path = "/app/app/templates/"
 
+
+templates_base_path = "/app/app/templates/"
 document_type_file_map = {"shared-cost-agreement": "shared_cost_agreement_template.docx"}
+
 
 
 class GenerateApplicationDocumentResource(Resource, UserMixin):
@@ -30,15 +35,13 @@ class GenerateApplicationDocumentResource(Resource, UserMixin):
 
         if not os.path.isfile(template_path):
             raise Exception('template file not found')
+            
+        token_data = {
+            "template_path" : template_path,
+            "application_guid": str(application_guid),
+            "generation":True
+        }
 
-        docgen_resp = DocumentGeneratorService.generate_document_and_stream_response(
-            template_path, application._doc_gen_json)
-        # Return the generated document
-        current_app.logger.info(docgen_resp.headers)
-
-        file_gen_resp = Response(
-            stream_with_context(docgen_resp.iter_content(chunk_size=2048)),
-            headers=dict(docgen_resp.headers))
-
-        current_app.logger.info(file_gen_resp.__dict__)
-        return file_gen_resp
+        token_guid = str(uuid.uuid4())
+        cache.set(DOWNLOAD_TOKEN(token_guid), token_data, TIMEOUT_5_MINUTES)
+        return {'token_guid': token_guid}
