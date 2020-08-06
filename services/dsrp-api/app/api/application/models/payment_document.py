1d83e7d4c532efb66055bb18c148166a2dfd1f6f
Luke Mitchell <lukegamitchell@gmail.com>
2020-08-06 14:54:08 -0700

    Add methods for getting PRF content in text format, and use those for file upload and finance email attachment

--- services/dsrp-api/app/api/application/models/payment_document.py
+++ services/dsrp-api/app/api/application/models/payment_document.py
@@ -44,12 +44,12 @@ class PaymentDocument(AuditMixin, Base):
             invoice_number = f'{agreement_number}-{payment_phase}-{amount_generated + 1}'
             return invoice_number
 
-        def upload_content_json():
-            document_name = f'{self.invoice_number}_{self.payment_document_code.lower()}.json'
+        def upload_contents():
+            document_name = f'{self.invoice_number}_{self.payment_document_code.lower()}.txt'
             file_path = f'{self.application.guid}/{self.payment_document_code.lower()}/{document_name}'
             try:
                 self.object_store_path = ObjectStoreStorageService().upload_string(
-                    self.content_json, file_path)
+                    self.content_text, file_path)
                 self.document_name = document_name
                 self.upload_date = datetime.utcnow()
             except Exception as e:
@@ -59,7 +59,7 @@ class PaymentDocument(AuditMixin, Base):
         self.application = application
         self.payment_document_type = PaymentDocumentType.find_by_payment_document_code(
             self.payment_document_code)
-        upload_content_json()
+        upload_contents()
 
     class _ModelSchema(Base._ModelSchema):
         document_guid = fields.String(dump_only=True)
@@ -155,5 +155,29 @@ class PaymentDocument(AuditMixin, Base):
         return json.dumps(content_json, indent=4)
 
     @hybrid_property
+    def content_text(self):
+        content_json = json.loads(self.content_json)
+        text = (f'Document Type\n{content_json["document_type"]}\n\n'
+                f'Invoice Number\n{content_json["invoice_number"]}\n\n'
+                f'Supplier Name\n{content_json["supplier_name"]}\n\n'
+                f'Supplier Address\n{content_json["supplier_address"]}\n\n'
+                f'PO Number\n{content_json["po_number"]}\n\n'
+                f'Account Coding\n{content_json["account_coding"]}\n\n'
+                f'Qualified Receiver\n{content_json["qualified_receiver_name"]}\n\n'
+                f'Date Payment Authorized\n{content_json["date_payment_authorized"]}\n\n'
+                f'Expense Authority\n{content_json["expense_authority_name"]}\n\n'
+                f'Payment Details\n'
+                f'Agreement Number\t\tUnique ID\t\tAmount\n')
+
+        for payment_detail in content_json['payment_details']:
+            text += f'{payment_detail["agreement_number"]}\t\t{payment_detail["unique_id"]}\t\t{payment_detail["amount"]}\n'
+
+        return text
+
+    @hybrid_property
     def content_json_as_bytes(self):
         return io.BytesIO(bytearray(self.content_json, 'utf-8'))
+
+    @hybrid_property
+    def content_text_as_bytes(self):
+        return io.BytesIO(bytearray(self.content_text, 'utf-8'))
