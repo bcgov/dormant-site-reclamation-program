01051be2d3af6719c9e6fb26f82d447fe72cc1dd
Luke Mitchell <lukegamitchell@gmail.com>
2020-08-13 10:10:26 -0700

    Update V2020.08.12.14.17__create_contracted_work_payment_info_and_payment_status_tables.sql

--- migrations/sql/V2020.08.12.14.17__create_contracted_work_payment_info_and_payment_status_tables.sql
+++ migrations/sql/V2020.08.12.14.17__create_contracted_work_payment_info_and_payment_status_tables.sql
@@ -1,10 +1,8 @@
--- WIP
-
 CREATE TABLE IF NOT EXISTS contracted_work_payment_status
 (
     contracted_work_payment_status_code varchar PRIMARY KEY,
     description varchar NOT NULL,
-	long_description varchar NOT NULL,
+    long_description varchar NOT NULL,
     active boolean DEFAULT true NOT NULL,
     create_user varchar NOT NULL,
     create_timestamp timestamp NOT NULL DEFAULT now(),
@@ -12,42 +10,48 @@ CREATE TABLE IF NOT EXISTS contracted_work_payment_status
     update_timestamp timestamp NOT NULL DEFAULT now()
 );
 
+ALTER TABLE contracted_work_payment_status OWNER TO dsrp;
+
 INSERT INTO TABLE contracted_work_payment_status (
-	contracted_work_payment_status,
+    contracted_work_payment_status,
     description,
-	long_description,
+    long_description,
     create_user,
     update_user
 )
 VALUES 
     ('INFORMATION_REQUIRED', 'Information Required', 'The applicant is able to edit and provide information for this work item in order for it to be reviewed by an admin.', 'system', 'system'),
-	('READY_FOR_REVIEW', 'Ready for Review', 'The applicant has indicated that they have provided all required information and that this work item is ready to be reviewed by an admin.')
+    ('READY_FOR_REVIEW', 'Ready for Review', 'The applicant has indicated that they have provided all required information and that this work item is ready to be reviewed by an admin.')
     ('UNDER_REVIEW', 'Under Review', 'The admin has indicated that they have begun reviewing the provided information on the work item.', 'system', 'system'),
-    ('APPROVED', 'Approved', 'The admin has indicated that the work item was approved and a payment request form was sent to Finance.', 'system', 'system')	
+    ('APPROVED', 'Approved', 'The admin has indicated that the work item was approved and a payment request form was sent to Finance.', 'system', 'system')    
 ON CONFLICT DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS contracted_work_payment
 (
-	contracted_work_payment_info_id SERIAL PRIMARY KEY,
-	application_guid uuid NOT NULL,
+    contracted_work_payment_id SERIAL PRIMARY KEY,
+    application_guid uuid NOT NULL,
     work_id varchar NOT NULL UNIQUE,
 
-	interim_payment_status_code varchar NOT NULL DEFAULT 'INFORMATION_REQUIRED',
-	final_payment_status_code varchar NOT NULL DEFAULT 'INFORMATION_REQUIRED',
+    interim_payment_status_code varchar NOT NULL DEFAULT 'INFORMATION_REQUIRED',
+    final_payment_status_code varchar NOT NULL DEFAULT 'INFORMATION_REQUIRED',
     interim_actual_est_cost numeric(14, 2),
     final_actual_est_cost numeric(14, 2),
-	interim_total_hours_worked_to_date integer,
-	final_total_hours_worked_to_date integer,
-	interim_total_people_employed_to_date integer,
-	final_total_people_employed_to_date integer,
-
-	-- TODO: interim/final EoC document table IDs, other document IDs...
-	
+    interim_total_hours_worked_to_date integer,
+    final_total_hours_worked_to_date integer,
+    interim_total_people_employed_to_date integer,
+    final_total_people_employed_to_date integer,
+    final_eoc_application_document_guid uuid,
+    interim_eoc_application_document_guid uuid,
+    
     create_user varchar NOT NULL,
     create_timestamp timestamp NOT NULL DEFAULT now(),
     update_user varchar NOT NULL,
-    update_timestamp timestamp NOT NULL DEFAULT now()
+    update_timestamp timestamp NOT NULL DEFAULT now(),
 
-	FOREIGN KEY (interim_payment_status_code) REFERENCES contracted_work_payment_status(contracted_work_payment_status_code) DEFERRABLE INITIALLY DEFERRED,
-	FOREIGN KEY (final_payment_status_code) REFERENCES contracted_work_payment_status(contracted_work_payment_status_code) DEFERRABLE INITIALLY DEFERRED
+    FOREIGN KEY (interim_payment_status_code) REFERENCES contracted_work_payment_status(contracted_work_payment_status_code) DEFERRABLE INITIALLY DEFERRED,
+    FOREIGN KEY (final_payment_status_code) REFERENCES contracted_work_payment_status(contracted_work_payment_status_code) DEFERRABLE INITIALLY DEFERRED,
+    FOREIGN KEY (final_eoc_application_document_guid) REFERENCES application_document(application_document_guid) DEFERRABLE INITIALLY DEFERRED,
+    FOREIGN KEY (interim_eoc_application_document_guid) REFERENCES application_document(application_document_guid) DEFERRABLE INITIALLY DEFERRED
 );
+
+ALTER TABLE contracted_work_payment OWNER TO dsrp;
