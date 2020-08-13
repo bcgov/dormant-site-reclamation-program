e760807caff8bc0eea6f9ee96be426ef3587eb5c
Luke Mitchell <lukegamitchell@gmail.com>
2020-08-13 09:27:21 -0700

    Update V2020.08.11.16.11__update_application_status_record.sql

--- migrations/sql/V2020.08.11.16.11__update_application_status_record.sql
+++ migrations/sql/V2020.08.11.16.11__update_application_status_record.sql
@@ -1,10 +1,5 @@
 UPDATE application_status
 SET
-	application_status_code = 'APPROVED',
 	description = 'Approved',
 	long_description = 'The application has been approved and is eligible to receive funding in the program.'
-WHERE application_status_code = 'FIRST_PAY_APPROVED';
-
-UPDATE application_status_change
-SET application_status_code = 'APPROVED'
 WHERE application_status_code = 'FIRST_PAY_APPROVED';
