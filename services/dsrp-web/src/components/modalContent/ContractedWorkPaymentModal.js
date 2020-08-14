619116c54205d38009b4da8c03112f43462bea85
Luke Mitchell <lukegamitchell@gmail.com>
2020-08-14 16:06:13 -0700

    WIP Contracted Work Payment modal/forms, rename vars, host EoC template, improve File Upload component,

--- services/dsrp-web/src/components/modalContent/ContractedWorkPaymentModal.js
+++ services/dsrp-web/src/components/modalContent/ContractedWorkPaymentModal.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import InterimContractedWorkPaymentForm from "@/components/forms/InterimContractedWorkPaymentForm";
 import { Row, Col, Typography, Table, Icon, Button, Tabs } from "antd";
+import InterimContractedWorkPaymentForm from "@/components/forms/InterimContractedWorkPaymentForm";
 
 const { TabPane } = Tabs;
 
@@ -22,8 +22,8 @@ export class ContractedWorkPaymentModal extends Component {
             closeModal={this.props.closeModal}
           />
         </TabPane>
-        <TabPane tab="Final Payment" key="1"></TabPane>
-        <TabPane tab="Interim Progress Report" key="2"></TabPane>
+        <TabPane tab="Final Payment" key="1" />
+        <TabPane tab="Interim Progress Report" key="2" />
       </Tabs>
     );
   };
