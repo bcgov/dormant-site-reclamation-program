6c1825d58ff3483bcf5d17366ff11f5a793cc53e
Luke Mitchell <lukegamitchell@gmail.com>
2020-08-20 15:22:06 -0700

    Fix bug with custom pagination function

--- services/dsrp-api/app/api/utils/helpers.py
+++ services/dsrp-api/app/api/utils/helpers.py
@@ -2,7 +2,7 @@ import math
 
 
 def apply_pagination_to_records(records, page_number, page_size):
-    total_records = len(records[:page_size])
+    total_records = len(records)
     total_pages = math.ceil(len(records) / page_size)
     items_per_page = min(total_records, page_size)
     paginated_records = records[(page_number - 1) * page_size:page_number * page_size]
