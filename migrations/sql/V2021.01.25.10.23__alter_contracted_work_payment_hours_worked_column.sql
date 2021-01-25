e34f253038e1d3ae99e3783d50425ca5cbf64b3b
Luke Mitchell <lukegamitchell@gmail.com>
2021-01-25 10:46:50 -0800

    Update precision/scale for hours worked

--- migrations/sql/V2021.01.25.10.23__alter_contracted_work_payment_hours_worked_column.sql
+++ migrations/sql/V2021.01.25.10.23__alter_contracted_work_payment_hours_worked_column.sql
@@ -1,3 +1,3 @@
 ALTER TABLE contracted_work_payment 
-ALTER COLUMN interim_total_hours_worked_to_date TYPE numeric,
-ALTER COLUMN final_total_hours_worked_to_date TYPE numeric;
+ALTER COLUMN interim_total_hours_worked_to_date TYPE numeric(14, 2),
+ALTER COLUMN final_total_hours_worked_to_date TYPE numeric(14, 2);
